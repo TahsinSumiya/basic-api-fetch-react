233bcbe862dbab22212063551cfaebf659fcc1b4
TahsinSumiya <tahsinadity187@gmail.com>
2022-11-15 22:35:18 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios"
 
 function App() {
+  const url = 'https://api.chucknorris.io/jokes/random';
+  const [joke,setJoke]=useState("")
+ 
+const fetchData = async ()=>{
+  const res = await fetch(url);
+  const data =await res.json();
+  setJoke(data.value)
+  console.log(data)
+}
+
+useEffect (()=>{
+fetchData(url)
+},[])
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    
+    <h1 className="head-1">Jokes fetching api</h1>
+    <div className="box">
+       <button className="btn" onClick={()=>fetchData()}>click to get new joke</button>
+       <h1 className="text">{joke}</h1>
     </div>
-  );
+    </>
+  )
 }
 
 export default App;
